c947422672e98c7f54d01b7b76eaaf0612e5052e
gjx <gjx@1502660122.com>
2022-11-17 11:42:53 +0800

    增加注释

--- Springmvc-05-ssm/src/main/java/com/gjx/controller/BookController.java
+++ Springmvc-05-ssm/src/main/java/com/gjx/controller/BookController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//控制层
 @RestController
 @RequestMapping("/books")
 public class BookController {
